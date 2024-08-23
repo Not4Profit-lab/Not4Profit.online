ab6af87a3ff7a504bfe8f71160f49f98bde7491a
Admin-verinsure <sangitabhuva05@gmail.com>
2024-08-23 15:02:00 +1200

    update ldap_module manifest.py file

--- addons/LDAP-Reset-Password-main/__manifest__.py
+++ addons/LDAP-Reset-Password-main/__manifest__.py
@@ -3,7 +3,7 @@
     'summary': 'Add LDAP Reset Password functionality',
     'description': 'A module to allow for a User to reset their password in LDAP from the reset password form.',
     'version': '1.0',
-    'author': 'Nathan Stocking',
+    'author': 'Verinsure',
     'depends': ['auth_ldap','base', 'membership', 'rotary_project_map'],
     'data': [
         'reset_ldap_password.xml',
