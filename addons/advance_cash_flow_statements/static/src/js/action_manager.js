ac7991b26599fb889264772fd9d443b3a07d326c
Admin-verinsure <sangitabhuva05@gmail.com>
2024-10-07 14:54:34 +1300

    changed action_manager.js

--- addons/advance_cash_flow_statements/static/src/js/action_manager.js
+++ addons/advance_cash_flow_statements/static/src/js/action_manager.js
@@ -2,15 +2,18 @@
 import { registry } from "@web/core/registry";
 import { BlockUI } from "@web/core/ui/block_ui";
 import { download } from "@web/core/network/download";
-// This function is responsible for generating and downloading an XLSX report.
-registry.category("ir.actions.report handlers").add("xlsx", async function (action){
-    if (action.report_type === 'xlsx') {
-        const blockUI = new BlockUI();
-        await download({
-            url: '/xlsx_reports',
-            data: action.data,
-            complete: () => unblockUI,
-            error: (error) => self.call('crash_manager', 'rpc_error', error),
-        });
-    }
-});
+
+// Check if 'xlsx' is already registered
+if (!registry.category("ir.actions.report handlers").contains("xlsx")) {
+    registry.category("ir.actions.report handlers").add("xlsx", async function (action) {
+        if (action.report_type === 'xlsx') {
+            const blockUI = new BlockUI();
+            await download({
+                url: '/xlsx_reports',
+                data: action.data,
+                complete: () => blockUI.unblock(),
+                error: (error) => self.call('crash_manager', 'rpc_error', error),
+            });
+        }
+    });
+}
